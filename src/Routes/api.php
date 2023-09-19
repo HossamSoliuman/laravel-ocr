c9f19442df0a9cb4941772f6e810f81ebf4094c3
hossam <hossam0soliuman@gmail.com>
2023-09-20 02:07:55 +0300

    update readme

--- src/Routes/api.php
+++ src/Routes/api.php
@@ -5,4 +5,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('api')->group(function () {
     Route::post('offline-ocr/image', [OfflineOcrController::class, 'image']);
+    Route::get('offline-ocr/languages', [OfflineOcrController::class, 'languages']);
 });
